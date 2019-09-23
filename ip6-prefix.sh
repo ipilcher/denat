b2c8f9c1959c1896cdff81ba0f89d9a05b545ec7
Ian Pilcher <arequipeno@gmail.com>
2019-09-23 04:47:00 -0500

    Rewrite dhclidnt IPv6 prefix hook
    * Wrap logic in function to avoid calling exit
    * Create rules/route on new boot

--- ip6-prefix.sh
+++ ip6-prefix.sh
@@ -1,40 +1,51 @@
-#!/bin/sh
+ip6_prefix() {
 
-[ "$interface" = bond0.256 ] || exit
-[ "$old_ip6_prefix" = "$new_ip6_prefix" ] && exit
+    [ "$interface" = bond0.256 ] || return
+    [ -z "$old_ip6_prefix" -a -z "$new_ip6_prefix" ] && return
 
+    # We always write a new state file (which will contain only a blank line if
+    # we don't have prefix).
+    echo "$new_ip6_prefix" > /run/dhclient6-prefix.new
+    mv /run/dhclient6-prefix.new /run/dhclient6-prefix
 
-#
-# The prefix has changeid (somehow), so clear the iptables chain, remove
-# the old route (if possible/applicable), and write the state file.
-#
+    # Flush the iptables rules; we'll (re-)create them if we have a valid new
+    # prefix.  (*New* incoming connections will be blocked in the interim.)
+    /usr/sbin/ip6tables -F FWD-INET6-IN
 
-echo "$new_ip6_prefix" > /run/dhclient6-prefix.new
-mv /run/dhclient6-prefix.new /run/dhclient6-prefix
+    # Do we have a new prefix?
+    if [ -n "$new_ip6_prefix" ] then
 
-/usr/sbin/ip6tables -F FWD-INET6-IN
+	# Is it the right size?
+	if [ ${new_ip6_prefix#*/} = 56 ] ; then
 
-if [ -n "$old_ip6_prefix" ] ; then
+	    # We have a usable prefix; (re-)create the iptables rules.
+	    local ASTERISK_ADDR=${new_ip6_prefix%00::/56}ff::2/128
+	    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 80 -j ACCEPT
+	    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 443 -j ACCEPT
+	    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 32698 -j ACCEPT
+	    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 32789:32790 -j ACCEPT
+
+	    # Create the route, if it doesn't already exist.
+	    if [ -z "$(/usr/sbin/ip -6 route show $new_ip6_prefix)" ] ; then
+        	/usr/sbin/ip -6 route add $new_ip6_prefix via fd00:dcaf:bad:ff::1
+	    fi
+	else
+	    /usr/bin/logger "ip6_prefix: prefix length not 56; ignoring"
+	fi
+    else
+	/usr/bin/logger "ip6_prefix: removing old prefix: $old_ip6_prefix"
+    fi
+
+    # If there is no old prefix, we're done.
+    [ -z "$old_ip6_prefix" ] && return
+
+    # If the prefix didn't change, we're done.
+    [ "$old_ip6_prefix" = "$new_ip6_prefix" ] && return
+
+    # Clear the old route, if it exists.
     if [ -n "$(/usr/sbin/ip -6 route show $old_ip6_prefix)" ] ; then
 	/usr/sbin/ip -6 route del $old_ip6_prefix
     fi
-fi
+}
 
-
-#
-# If a new prefix was passed, add the iptables rules and route (if
-# the latter doesn't already exist).
-#
-
-if [ -n "$new_ip6_prefix" ] ; then
-
-    ASTERISK_ADDR=${new_ip6_prefix%00::/56}ff::2/128
-    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 80 -j ACCEPT
-    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 443 -j ACCEPT
-    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 32698 -j ACCEPT
-    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 32789:32790 -j ACCEPT
-
-    if [ -z "$(/usr/sbin/ip -6 route show $new_ip6_prefix)" ] ; then
-	/usr/sbin/ip -6 route add $new_ip6_prefix via fd00:dcaf:bad:ff::1
-    fi
-fi
+ip6_prefix
