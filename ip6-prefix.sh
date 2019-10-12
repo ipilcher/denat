81619f4c63129008bf15851807e70107536e5afd
Ian Pilcher <arequipeno@gmail.com>
2019-10-12 13:11:14 -0500

    Handle /48, /52, or /56 prefixes

--- ip6-prefix.sh
+++ ip6-prefix.sh
@@ -1,3 +1,47 @@
+padword() {
+    local WORD=$1
+    while [ $(expr length "$WORD") -lt 4 ] ; do
+	WORD=0${WORD}
+    done
+    echo $WORD
+}
+
+v6merge() {
+
+    local PREFIX=$1
+    local HOST=$2
+
+    PREFIXLEN=${PREFIX#*/}
+
+    if [ $PREFIXLEN -gt 56 -o $PREFIXLEN -lt 48 ] ; then
+	/usr/bin/logger "ip6_prefix: ignoring invalid prefix: $PREFIX"
+	return 1
+    fi
+
+    if [ $((PREFIXLEN%4)) != 0 ] ; then
+	/usr/bin/logger "ip6_prefix: ignoring invalid prefix: $PREFIX"
+	return 1
+    fi
+
+    local W1=${PREFIX%%:*}
+    W1=$(padword $W1)
+
+    local W2=${PREFIX#*:}
+    W2=${W2%%:*}
+    W2=$(padword $W2)
+
+    local W3=${PREFIX#*:*:}
+    W3=${W3%%:*}
+    W3=$(padword $W3)
+
+    local W4=${PREFIX#*:*:*:}
+    W4=${W4%%:*}
+    W4=$(padword $W4)
+    W4=${W4%??}
+
+    echo "${W1}:${W2}:${W3}:${W4}${HOST}/128"
+}
+
 ip6_prefix() {
 
     [ "$interface" = bond0.256 ] || return
@@ -15,11 +59,11 @@ ip6_prefix() {
     # Do we have a new prefix?
     if [ -n "$new_ip6_prefix" ] ; then
 
-	# Is it the right size?
-	if [ ${new_ip6_prefix#*/} = 56 ] ; then
+	# Create an address for iptables rules (also checks prefix length)
+	local ASTERISK_ADDR
+	if ASTERISK_ADDR=$(v6merge $new_ip6_prefix ff::1) ; then
 
 	    # We have a usable prefix; (re-)create the iptables rules.
-	    local ASTERISK_ADDR=${new_ip6_prefix%00::/56}ff::1/128
 	    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p icmpv6 -j ACCEPT
 	    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 80 -j ACCEPT
 	    /usr/sbin/ip6tables -A FWD-INET6-IN -d $ASTERISK_ADDR -p tcp -m tcp --dport 443 -j ACCEPT
@@ -30,8 +74,6 @@ ip6_prefix() {
 	    if [ -z "$(/usr/sbin/ip -6 route show $new_ip6_prefix)" ] ; then
         	/usr/sbin/ip -6 route add $new_ip6_prefix via fd00:dcaf:bad:ff::1
 	    fi
-	else
-	    /usr/bin/logger "ip6_prefix: prefix length not 56; ignoring"
 	fi
     else
 	/usr/bin/logger "ip6_prefix: removing old prefix: $old_ip6_prefix"
